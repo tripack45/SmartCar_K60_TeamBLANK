c92a1c2a388d6c6aac5684fc6e4765cd2b1a7799
Yao Yue <patrickyao@sjtu.edu.cn>
2016-03-22 00:43:05 +0800

    A script to test algorithms on a single frame

---
+++ matlab_code/run_alg_on_single_frame.m
@@ -0,0 +1,37 @@
+%% inputs
+imgrow=67;
+imgcol=77;
+algrow=150;
+algcol=150;
+source=frame;
+i=2079;
+%1069,1075,1111,1081,1089
+%1213,1280,1293,1311,1324
+%1425,1429,1691,1790,2046
+%2121,2079,2136,2232,2244
+
+%% Set up the figures
+InitializeFigures;
+
+%% Applying Algorithms and Drawing them
+tic;
+last_draw_toc=0;
+last_frame_toc=0;
+
+%Load A Frame
+img=source(:,:,i);
+
+%Apply Algorithms.
+[out algdir algspd]=alg(img);
+
+%Recall Logging Data
+dir=dirlog(i);
+spd=spdlog(i);
+tacho=tacholog(i);
+
+%Frame Number
+frameNum=i;
+UpdateFigures;
+
+last_frame_toc=toc;
+%drawnow nocallbacks;
