1bc81ba85b50f18f8149bb1916713617e2f41dc0
Yao Yue <patrickyao@sjtu.edu.cn>
2016-03-21 15:08:20 +0800

    Updates the process of drawing

--- matlab_code/main.m
+++ matlab_code/main.m
@@ -13,6 +13,9 @@ end
 imgrow=67;
 imgcol=77;
 extraInfoSize=6;
+algrow=150;
+algcol=150;
+frameNum=0;
 %COM4, BuadRate 115200, DataBits 8
 %fclose(com);
 disp('Reopening Com');
@@ -30,44 +33,11 @@ disp('Preparing to buffer')
 
 
 %% Graphic Initializations
-try
-    set(h,'Visible','on');
-    figure(h);
-catch
-    h=figure;
-end
-set(h,'Renderer','OpenGL');
-set(h,'Position',[58,126,1300,557]);
+InitialzieFigures;
 set(h,'KeyPressFcn',@keyboard_callback);
-ax1=subplot(1,2,1);
-imin=image(ones(imgrow,imgcol));
-colormap(ax1,gray);
-axis equal;
-axis manual;
-text(0,-10,'DIR:','Color','Black');
-text(20,-10,'SPD:','Color','Black');
-text(40,-10,'TCH:','Color','Black');
-
-t3=text( 7,-10,'000','Color','Black');
-t4=text( 27,-10,'000','Color','Black');
-t5=text( 47,-10,'000','Color','Black');
-
-ax2=subplot(1,2,2);
-imalg=image(ones(150,150));
-colormap(ax2,colorcube);
-axis equal;
-axis manual;
-caxis manual;
-caxis([0,255]);
-text(0,-10,'DIR:','Color','Black');
-text(20,-10,'SPD:','Color','Black');
-t1=text( 7,-10,'000','Color','Black');
-t2=text( 27,-10,'000','Color','Black');
-%text(-30,-10,'Time:','Color','Black');
-%t3=text(-23,-10,'000','Color','Black');
 
 %%
-frame=ones(imgrow,imgcol,1);
+
 disp('Recieving');
 b_buffer=[];
 while 1
@@ -111,39 +81,35 @@ while 1
     end
     %disp('drawing');
     
-    %fwrite(com,[172,160,0,0,76]);
-    
     if length(indata)==imgcol*imgrow+extraInfoSize
-        spdu8=indata(end-5:end-4);
-        diru8=indata(end-3:end-2);
-        tachou8=indata(end-1:end);
+        
+        %Collecting Extra Data
+        E=imgcol*imgrow;
+        spdu8=indata(E+1:E+2);
+        diru8=indata(E+3:E+4);
+        tachou8=indata(E+5:E+6);
         spd=typecast(uint8(spdu8),'int16');
         dir=typecast(uint8(diru8),'int16');
         tacho=typecast(uint8(tachou8),'int16');
-        set(t3,'String',num2str(dir));
-        set(t4,'String',num2str(double(spd)/100*27));
-        set(t5,'String',num2str(tacho));
         
-        indata=indata(1:end-extraInfoSize);
+        %Collecting Image Data
+        indata=indata(1:imgcol*imgrow);
         img=reshape(indata,[imgcol,imgrow]);
         img=img.';
-        set(imin,'CData',img);
-        frame(:,:,end+1)=img;
+        
+        %Apply the algorithms
+        [out algdir algspd]=alg(img);
+        
+        %Logging the results
+        imglog(:,:,end+1)=img;
         dirlog(end+1)=dir;
         spdlog(end+1)=spd;
         tacholog(end+1)=tacho;
         
-        [out dir spd]=alg(img);
-        set(imalg,'CData',out);
-        set(t1,'String',num2str(dir));
-        set(t2,'String',num2str(spd));
-        try
-            drawnow nocallbacks;
-        catch
-            drawnow;
-        end
-        %drawnow nocallbacks ;
-        %toc;
+        %update FrameNumer
+        frameNum=frameNum+1;
+        
+        UpdateFigures;
     else
         disp('Invalid frame size!');
     end
