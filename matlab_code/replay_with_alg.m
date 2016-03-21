057861f23c55d3ae872b374ced4e478a785c846e
Yao Yue <patrickyao@sjtu.edu.cn>
2016-03-21 15:12:38 +0800

    Update method of drawing

--- matlab_code/replay_with_alg.m
+++ matlab_code/replay_with_alg.m
@@ -1,61 +1,37 @@
 %% inputs
 imgrow=67;
 imgcol=77;
+algrow=150;
+algcol=150;
 source=frame;
-fps=3;
-start=1000;
+fps=20;
+start=500;
 
 %% Set up the figures
-try
-    set(h,'Visible','on');
-    figure(h);
-catch
-    h=figure;
-end
-set(h,'Renderer','OpenGL');
-set(h,'Position',[58,126,1300,557]);
-
-ax1=subplot(1,2,1);
-imrep=image(ones(imgrow,imgcol));
-colormap(ax1,gray);
-axis equal;
-axis manual;
-
-ax2=subplot(1,2,2);
-imalg=image(ones(150,150));
-colormap(ax2,colorcube);
-axis equal;
-axis manual;
-caxis manual;
-caxis([0,255]);
-text(0,-10,'DIR:','Color','Black');
-text(20,-10,'SPD:','Color','Black');
-text(40,-10,'SP:','Color','Black');
-t1=text( 7,-10,'000','Color','Black');
-t2=text( 27,-10,'000','Color','Black');
-t3=text( 47,-10,'000','Color','Black');
-
-text(-30,-10,'Time:','Color','Black');
-t255=text(-23,-10,'000','Color','Black');
+InitializeFigures;
 
 %% Applying Algorithms and Drawing them
 tic;
 last_draw_toc=0;
 last_frame_toc=0;
 for i=start:size(source,3)
-    set(t255,'String',num2str(i/50));
-     [out dir spd]=alg(source(:,:,i));
-    set(imrep,'CData',source(:,:,i));
-    set(imalg,'CData',out);
-    set(t1,'String',num2str(dir));
-    set(t2,'String',num2str(spd));
-    set(t3,'String',num2str(0));
+    
+    %Load A Frame
+    img=source(:,:,i);
+    
+    %Apply Algorithms.
+    [out algdir algspd]=alg(img);
+    
+    %Recall Logging Data
+    dir=dirlog(i);
+    spd=spdlog(i);
+    tacho=tacholog(i);
+    
+    %Frame Number
+    FrameNum=i;
+    
     if (toc-last_draw_toc)>(1/20) 
-        try
-            drawnow nocallbacks;
-        catch 
-            drawnow;
-        end
+        UpdateFigures;
         last_draw_toc=toc;
     end
     while (toc-last_frame_toc)<(1/fps)
