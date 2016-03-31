81026137c6d8b867c122aaad9eb6c712c93ea0f9
Yao Yue <patrickyao@sjtu.edu.cn>
2016-03-31 15:53:32 +0800

    A fix for matlab typo

--- matlab_code/main.m
+++ matlab_code/main.m
@@ -33,13 +33,17 @@ disp('Preparing to buffer')
 
 
 %% Graphic Initializations
-InitialzieFigures;
+InitializeFigures;
 set(h,'KeyPressFcn',@keyboard_callback);
 
 %%
 
 disp('Recieving');
 b_buffer=[];
+imglog=[];
+tacholog=[];
+spdlog=[];
+dirlog=[];
 while 1
     if length(b_buffer)>3*imgcol*imgrow
         b_buffer=[];
