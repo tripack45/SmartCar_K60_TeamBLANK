f0626c69d417a682c9b230d130f6b74ff02771a2
Jerry Yang <yhxlele@gmail.com>
2016-04-07 00:14:38 +0800

    Fix CircleStateHandler Make some typos :)

--- source/CircleStateHandler.c
+++ source/CircleStateHandler.c
@@ -1,5 +1,5 @@
 void CircleStateHandler(struct controlStateStruct* state){
-  if (state.lineMSE < MSE_RATIO * state.circleMSE){
+  if (state->lineMSE < MSE_RATIO * state->circleMSE){
     //TODO: LinearStateHandler(state);
     //return;
   }else{
@@ -7,22 +7,22 @@ void CircleStateHandler(struct controlStateStruct* state){
     s16 theta;
     s16 speed;
     s8 direction = 1;
-    offset=Isqrt((currentState.carPosX-currentState.circleX)^2+(currentState.carPosY-currentState.circleY)^2)-currentState.circleRadius;
-    If (offset < OFFSET_THRES && offset > -OFFSET_THRES){
-      theta = direction * CURVE_DIR_RATIO / currentState.circleRadius + OFFSET_DIR_RATIO * offset;
+    offset=Isqrt((state->carPosX-state->circleX)^2+(state->carPosY-state->circleY)^2)-state->circleRadius;
+    if (offset < OFFSET_THRES && offset > -OFFSET_THRES){
+      theta = direction * CURVE_DIR_RATIO / state->circleRadius + OFFSET_DIR_RATIO * offset;
       if (theta > DIR_MAX || theta < -DIR_MAX){
         currdir = direction * DIR_MAX;
         currspd = BASIC_SPD;
         return;
       }
-      speed = floor(CURVE_SPD_RATIO * currentState.circleRadius - OFFSET_SPD_RATIO * offset + BASIC_SPD)
+	  speed = floor(CURVE_SPD_RATIO * state->circleRadius - OFFSET_SPD_RATIO * offset + BASIC_SPD);
       if (speed > SPD_MAX) speed = SPD_MAX;
       if (speed < SPD_MIN) speed = SPD_MIN;
       currdir = theta;
       currspd = speed;
       return;
     }
-    If (offset < 0){
+    if (offset < 0){
       currdir = 0;
       currspd = SPD_MAX;
       return;
